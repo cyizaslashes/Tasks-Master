c3e4337d4c10bda7909f3f983d26e278b2021ae4
cyizaslashes <121486478+cyizaslashes@users.noreply.github.com>
2023-01-13 05:01:38 -0800

    Delete TASKS-API-master directory

--- TASKS-API-master/db/connect.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDB = async (url) =>{
-   await  mongoose.connect(url).then(()=>{
-    console.log("database connection established")
-   })
-}
-module.exports = connectDB
--- TASKS-API-master/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const {createUser, login} = require("../controllers/auth")
-const router = express.Router()
-
-router.post("/register", createUser)
-router.post("/login", login)
-
-module.exports = router
